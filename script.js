70a1a03c63a682786e0ee8d22d27133def990267
varshaa2k <67535245+varshaa2k@users.noreply.github.com>
2025-10-20 19:54:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,8 @@ fetch('data.json')
     data.experience.milestones.forEach(item => {
       let div = document.createElement('div');
       div.className = 'timeline-item';
-      div.innerHTML = `<h4>${item.role}</h4><p>${item.company}</p><span>${item.duration}</span>`;
+      let bulletPoints = item.details ? '<ul>' + item.details.map(d => `<li>${d}</li>`).join('') + '</ul>' : '';
+      div.innerHTML = `<h4>${item.role}</h4><p>${item.company}</p><span>${item.duration}</span>${bulletPoints}`;
       expContainer.appendChild(div);
     });
 
@@ -48,29 +49,31 @@ fetch('data.json')
     let projectsContainer = document.getElementById('projects-list');
     data.projects.forEach(project => {
       let techBadges = project.tech.map(t => `<span class="badge">${t}</span>`).join(" ");
-      let demoButton = project.demo ? `<a href="${project.demo}" target="_blank" class="btn btn-demo">Demo</a>` : '';
+      let demoButton = project.demo ? `<a href="${project.demo}" target="_blank" class="btn-demo">Demo</a>` : '';
       let div = document.createElement('div');
       div.className = 'card';
       div.innerHTML = `<h4>${project.title}</h4><p>${project.description}</p>
         <p>${techBadges}</p>
-        <a href="${project.github}" target="_blank" class="btn btn-github">GitHub</a> ${demoButton}`;
+        <a href="${project.github}" target="_blank" class="btn-github">GitHub</a> ${demoButton}`;
       projectsContainer.appendChild(div);
     });
 
     // Honours
     let honoursContainer = document.getElementById('honours-list');
     data.honours.forEach(h => {
-      let li = document.createElement('li');
-      li.textContent = h;
-      honoursContainer.appendChild(li);
+      let div = document.createElement('div');
+      div.className = 'honour-item';
+      div.textContent = h;
+      honoursContainer.appendChild(div);
     });
 
     // Hobbies
     let hobbiesContainer = document.getElementById('hobbies-list');
     data.hobbies.forEach(h => {
-      let li = document.createElement('li');
-      li.textContent = h;
-      hobbiesContainer.appendChild(li);
+      let div = document.createElement('div');
+      div.className = 'hobby-item';
+      div.textContent = h;
+      hobbiesContainer.appendChild(div);
     });
 
     // Languages
